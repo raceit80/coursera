6890fdc7c277e5f1e43f35ec9bf99168980cd987
Ravi Subramanian <ravi.subram@gmail.com>
2017-10-11 21:29:06 -0500

    rubypractice

--- rubyonrails/01_Intro_to_Ruby/practice/student-start/module3_1_assignment.rb
+++ rubyonrails/01_Intro_to_Ruby/practice/student-start/module3_1_assignment.rb
@@ -1,5 +1,34 @@
+
+
 require 'httparty'
+require 'pp'
+
+
+# Use the class methods to get down to business quickly
+
+response = HTTParty.get('http://food2fork.com/api/search?key=7326dfc69272a062e170ec938206dcfb&q=shredded%20chicken')
+
+puts response.body, response.code, response.message, response.headers.inspect
+
+pp ENV
+
 
 class Recipe
+	include HTTParty
+
+	base_uri 'http://food2fork.com/api/search'
+	default_params key: ENV['FOOD2FORK_KEY']	
+	format :json
+
+	#def initialize(term)
+    #@options = { query: { q: term } }
+  	#end
+
+	def self.for(term)
+		 @options = { query: { q: term } }
+    	 get("", @options)
+  	end
+
 
 end
+
--- rubyonrails/01_Intro_to_Ruby/practice/student-start/solution.rb
+++ rubyonrails/01_Intro_to_Ruby/practice/student-start/solution.rb
@@ -1,3 +1,6 @@
 require_relative "module3_1_assignment"
 
+#puts Recipe.for("chocolate")
+
 puts Recipe.for("chocolate")
+#puts recipe1.results
